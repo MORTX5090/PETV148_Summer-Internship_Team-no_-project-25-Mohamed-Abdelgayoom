f9ade36f29d9f59bc569462a34423a65fba3e534
MORTX5090 <mohamedieen2510@gmail.com>
2026-07-06 23:31:29 +0400

    Integrate Scanner Engine With Flask app and Validate Scan Route

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
 # Hi i'am MOHAMED ABDELGAYOOM this my branch (do'nt EDIT OR CHANGE ANYTHING) here
 
-from flask import Flask,request,render_template
+from flask import Flask,request,render_template,jsonify
+from scanner import scan_headers
+
 app = Flask(__name__)
+
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
+
+@app.route('/scan', methods=['POST'])
+def scan():
+    target_url = request.form.get('url')
+    if not target_url:
+        return jsonify({"success": False, "error": "URL parameter is missing"})
+    scan_result = scan_headers(target_url)
+    return jsonify(scan_result)
+
 if __name__ == '__main__':
     app.run(debug=True)
